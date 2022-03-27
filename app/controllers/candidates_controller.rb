76ce227563548d905952285fdb6d218d4c910680
gian910147 <gian910147@gmail.com>
2022-03-28 01:54:07 +0800

    show & next CSS decoration

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,32 @@
 class CandidatesController < ApplicationController
   def index
+    @candidates = Candidate.all
+  end
+
+  def show
+    @candidate = Candidate.find_by(id: params[:id])
   end
 
   def new
+    @candidate = Candidate.new
   end
 
   def create
+
+    @candidate = Candidate.new(candidate_params)
+
+    if @candidate.save
+      flash[:notice] = "Candidate created!"
+      redirect_to "/candidates" # OK
+    else
+      render :new
+      # new.html.erb
+    end
+  end
+
+
+  private
+  def candidate_params
+    params.require(:candidate).permit(:name, :party, :age, :politics)
   end
 end

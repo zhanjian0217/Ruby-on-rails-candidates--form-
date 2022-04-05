3e41c7f5e61a08d45557039332291afb1cdf5a19
gian910147 <gian910147@gmail.com>
2022-04-05 22:09:55 +0800

    ActionRecord

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,10 +1,15 @@
 class CandidatesController < ApplicationController
+
+  before_action :find_candidate, except: [:new, :create, :index]
+                                 #only:[:show, :edit, :update, :destroy, :vote]
+
   def index
-    @candidates = Candidate.all
+    # @candidates = Candidate.all
+    @candidates = Candidate.young_people
+    # @candidates = Candidate.where("age < 40")
   end
 
-  def show
-    @candidate = Candidate.find_by(id: params[:id])
+  def show 
   end
 
   def new
@@ -16,51 +21,44 @@ class CandidatesController < ApplicationController
     @candidate = Candidate.new(candidate_params)
 
     if @candidate.save
-      flash[:notice] = "Candidate created!"
-      redirect_to "/candidates" # OK
+    redirect_to "/candidates" , notice: "Candidate created!" # OK
     else
-      render :new
-      # new.html.erb
+      render :new# new.html.erb
     end
   end
 
-  def edit
-    @candidate = Candidate.find_by(id: params[:id]) 
+  def edit 
   end
 
-  def update 
-    @candidate = Candidate.find_by(id: params[:id]) 
+  def update  
 
     if @candidate.update(candidate_params)
-      flash[:notice] = "Candidate created!"
-      redirect_to "/candidates" # OK
+      redirect_to "/candidates" , notice: "Candidate updated!"
     else
       render :edit
     end
   end
 
-  def destroy
-    @candidate = Candidate.find_by(id: params[:id]) 
+  def destroy 
     @candidate.destroy
-    
-    flash[:notice] = "Candidate deleted !!!!!"
-    redirect_to "/candidates"
+    redirect_to "/candidates" , notice: "Candidate deleted !!!!!"
   end
   
-  def vote
-    @candidate = Candidate.find_by(id: params[:id])
+  def vote 
 
     # VoteLog.create(candidate: @candidate, ip_address: request.remote_ip)
     
     @candidate.vote_logs.create(ip_address: request.remote_ip)
-
-
-    flash[:notice] = "Voted !!!!!" 
-    redirect_to "/candidates"
+    redirect_to "/candidates", notice: "Voted !!!!!"
   end
+
 
   private
   def candidate_params
     params.require(:candidate).permit(:name, :party, :age, :politics)
+  end
+
+  def find_candidate
+    @candidate = Candidate.find_by(id: params[:id])
   end
 end
--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,5 +2,9 @@ class Candidate < ApplicationRecord
   
   validates :name, presence: true
   has_many :vote_logs
-  
+ 
+  scope :young_people,  -> { where("age < 40") }
+  scope :order_than, -> (x) { unscope(:where).where("age > #{x}")  }
+
+  default_scope { where("age > 0") }
 end
